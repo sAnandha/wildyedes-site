f0d87484bfae957e94fa661df2fe3bda2176acb9
S.Anandha Nivas <115523613+sAnandha@users.noreply.github.com>
2024-11-24 23:56:49 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_G1VpOTesS', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '75hdrg99htgc80pu3ls3krc6vr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
