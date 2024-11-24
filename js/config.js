db2d34c7c2113620ace94bae86968ec3384c1902
S.Anandha Nivas <115523613+sAnandha@users.noreply.github.com>
2024-11-25 00:18:12 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'my_cognito', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'ap-south-1_euXN2dCTE', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'my-web-cog', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'ap-south-1_kzTppn1Fh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
